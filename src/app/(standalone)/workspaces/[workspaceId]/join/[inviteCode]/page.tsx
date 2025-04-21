85a3ea0248e19ea73cc693b918d02ae11acf3704
MohammedAdnan <MohammedAdnan.cs@hotmail.com>
2025-04-21 16:24:02 +0200

    fix landing page route

--- src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -3,12 +3,9 @@ import { getCurrent } from "@/features/auth/queries";
 import { redirect } from "next/navigation";
 import { WorkspaceIdJoinClient } from "./client";
 
-
-const WorkspaceIdJoinPage = async ()  => {
+export default async function JoinPage() {
     const user = await getCurrent();
-    if(!user) redirect("http://localhost:3000/landingpage");
+    if(!user) redirect(`${process.env.NEXT_PUBLIC_APP_URL}/landingpage`);
 
     return <WorkspaceIdJoinClient />;
 }
-
-export default WorkspaceIdJoinPage;
