ef53b673b5bf2d7db36106bb289478032ee71a3b
MohammedAdnan <MohammedAdnan.cs@hotmail.com>
2025-04-21 18:16:01 +0200

    fix routes

--- src/app/(mainpage)/landingpage/page.tsx
+++ src/app/(mainpage)/landingpage/page.tsx
@@ -1,9 +1,15 @@
 import {LandingPageClient} from "./client";
-
+import { getCurrent } from "@/features/auth/queries";
+import { redirect } from "next/navigation";
 export const dynamic = 'force-dynamic';
 
-export default function LandingPage() {
-    return (
-      <LandingPageClient user={null}/>
-    )
+const LandingPage = async () => {
+
+  const user = await getCurrent();
+  console.log(user, "user in landing page")
+  return (
+    <LandingPageClient user={user}/>
+  )
 }
+
+export default LandingPage;
